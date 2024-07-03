2af24287ce311aa45fe4240ee5c3c3771a277674
ShlomoV <shlomov@gmail.com>
2024-07-03 11:10:57 +0300

    Create script.js

---
+++ docs/script.js
@@ -0,0 +1,14 @@
+let currentSection = 0;
+const sections = document.querySelectorAll('.section');
+
+function navigate(direction) {
+    currentSection += direction;
+    if (currentSection < 0) {
+        currentSection = sections.length - 1;
+    } else if (currentSection >= sections.length) {
+        currentSection = 0;
+    }
+    sections.forEach((section, index) => {
+        section.style.transform = `translateX(${-100 * currentSection}%)`;
+    });
+}
